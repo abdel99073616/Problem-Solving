b95ef5dd4ebe2601cc9e4c7d10012a278be05fcb
Abdo Mohamed <70642398+abdel99073616@users.noreply.github.com>
2021-01-06 01:09:55 +0200

    Add files via upload

--- Two Sum/main.cpp
+++ Two Sum/main.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
-#include <algorithm>
+
 using namespace std;
 
 int main()
 {
-    string word;
-    cin>>word;
-    word[0] = toupper(word[0]);
-    cout<<word;
+    int arr[5] = {2,7,11,15};
+    int target = 9;
+    int j=0;
+    int arr2[5] = {0};
+    for(int i=0;i<5;i++){
+        if(arr[i] <= target){
+            arr2[j] = arr[i];
+            j++;
+        }
+    }
+
+    for(int i=0 ; i<5 ; i++)
+    {
+        if(arr2[i] + arr2[i+1] == target)
+        {
+            cout<< i << i+1 ;
+        }
+    }
     return 0;
 }
